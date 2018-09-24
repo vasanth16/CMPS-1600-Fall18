b4f5e2dba2f22a737cecebee3dd58040634a94e1
vasanth16 <vasanthrajvr@gmail.com>
2018-09-24 14:24:54 -0500

    Lab 3 part 1 completed

---
+++ Lab 3/Rectangle.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+
+
+class Rectangle{
+	private double len;
+	private	double wid;
+
+	public static int counter = 0;
+
+	private int rectangleID;
+	
+
+	public double area (){
+		return len * wid;
+	}
+
+	public double perimeter (){
+
+		return 2*len + 2*wid;
+	}
+
+	public double getLength(){ // getter for the length
+		return this.len;
+	}
+	public void setLength(int le){ // setter for length
+		this.len = le;
+	}
+
+
+	public double getHeight(){ // Getter for the width
+		return this.wid;
+	}
+	public void setHeight(int wi){ // setter for width
+		this.wid = wi; 
+	}
+
+	public Rectangle(double l, double w){
+		len = l;
+		wid = w;
+		
+
+		this.rectangleID = counter;
+		counter ++;
+	}
+	public Rectangle (){
+
+	}
+	public int getRectangleID(){
+		return this.rectangleID;
+	}
+
+	public int getCounter(){
+		return this.counter;
+	}
+	public String toString(){
+		//System.out.println("Your rectangle with the id "+ this.rectangleID +" has a length of "+ this.len +" units and a height of "+ this.wid +" units");
+		return "Your rectangle with the id "+this.getRectangleID()+" has a length of "+ this.getLength()+" units and a height of "+ this.getHeight()+" units";
+	}
+}
+
+class RectangleTester{
+	public static void main (String[] args){
+
+
+		Rectangle yent = new Rectangle(5,10);
+
+		System.out.println("Rectangle "+ yent.getRectangleID()+"'s Length is "+ yent.getLength());
+		System.out.println("Rectangle "+ yent.getRectangleID()+"'s width is "+ yent.getHeight());
+
+		System.out.println("The Area of the Rectangle is "+ yent.area());
+		System.out.println("The Perimeter of the Rectangle is "+ yent.perimeter());
+		
+		yent.setLength(20);
+
+		System.out.println("Rectangle "+ yent.getRectangleID()+"'s Length is "+ yent.getLength());
+		System.out.println("Rectangle "+ yent.getRectangleID()+"'s width is "+ yent.getHeight());
+		
+		System.out.println("The Area of the Rectangle is "+ yent.area());
+		System.out.println("The Perimeter of the Rectangle is "+ yent.perimeter());
+
+		Rectangle woke = new Rectangle(4,6);
+		System.out.println(woke.getCounter() + " rectangles have been created");
+
+		System.out.println("The id for yent rectangle is "+yent.getRectangleID());
+		System.out.println("The id for the woke rectangle is "+woke.getRectangleID());
+
+
+	}
+}
+
---
+++ Lab 3/Square.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+
+class Square extends Rectangle{
+	private int size = 0;
+
+	public int getSize(){
+		return this.size;
+	}
+	public Square (int s){
+		this.size = s;
+		this.setLength(size);
+		this.setHeight(size);
+	}
+	@Override
+	public String toString(){
+		//System.out.println("Your square with the id "+this.getRectangleID()+" has a length of "+ this.getLength()+" units and a height of "+ this.getHeight()+" units");
+		return "Your square with the id "+this.getRectangleID()+" has a length of "+ this.getLength()+" units and a height of "+ this.getHeight()+" units";
+	}
+
+	public boolean equals(Object other){
+		if (!(other instanceof Square)){
+			return false ;
+		}
+		Square s = (Square) other;
+		if (this.size == s.size){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+}
+
+
+class SquareTester {
+	public static void main(String[] args) {
+		Rectangle a = new Rectangle (5.0, 6.0);
+		Rectangle b = new Rectangle (5.0, 6.0);
+
+		Square c = new Square (5);
+		Square d = new Square (5);
+
+		if (a == b ){
+			System.out.println("The rectangles are Equal using ==");
+		}else {
+			System.out.println("The rectangles are not equal");
+		}
+
+		if (a.equals(b)){
+			System.out.println("The rectangles are Equal using equals");
+		}else{
+			System.out.println("The Retangles are not equal");
+		}
+
+		if (c == d){
+			System.out.println("The squares are Equal using ==");
+		}else{
+			System.out.println("The squares are not equal");
+		}
+
+		if (c.equals(d)){
+			System.out.println("The squares are equal using the equals method");
+		}else{
+			System.out.println("The squares are not equal");
+		}
+
+	}
+}
