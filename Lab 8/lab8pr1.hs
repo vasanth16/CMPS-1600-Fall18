ee7036fb0fa2357d7b87458f37cee1b20e18bf48
vasanth16 <vasanthrajvr@gmail.com>
2018-12-04 23:02:42 -0600

    Finished the start of lab 8

--- Lab 8/lab8pr1.hs
+++ Lab 8/lab8pr1.hs
@@ -22,16 +22,54 @@ l = ["Mr.", "Ms.", "Mrs.", "Dr.", "Prof.", "Rev."]
 
 pairs :: [String]
 
-pairs = [a , b , a++"and"++ b, b++"and"++a | a <- l, b <- l]
+pairs = [a | a <- l] ++ [a ++ " and " ++b| a <- l, b <- l]
 
 lengthOfPairs :: Int
 lengthOfPairs = length pairs
 
+natoAlp :: [(Char,String)]
+
+militaryWord :: [String]
+
+wordPassed :: String
+wordPassed = "DAY"
+
+natoAlp = [('A',"Alfa"), ('B',"Bravo"), ('C',"Charlie"), ('D',"Delta"), ('E',"Echo"), ('F',"Foxtrot"), ('G',"Golf"), ('H', "Hotel"), ('I', "India"), ('J', "Juliett"), ('K', "Kilo"), ('L', "Lima"), ('M', "Mike"), ('N', "November"), ('O', "Oscar"), ('P', "Papa"), ('Q', "Quebec"), ('R', "Romeo"), ('S', "Sierra"), ('T',"Tango"), ('U',"Uniform"), ('V',"Victor"), ('W',"Whiskey"), ('X',"X-ray"), ('Y',"Yankee"), ('Z',"Zulu")]
+
+militaryWord = [ snd(natoAlp !! x)  | x <- [0..25] , y <- wordPassed, y == fst(natoAlp !! x)]
+
+
+findremainder x y = x `mod` y
+
+
+isEven x = if x `mod` 2 == 0
+			then True
+			else False
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) = if x < y
+						then x:(merge xs (y:ys))
+						else y:(merge (x:xs) ys)
+
+removeMultiple :: [Int] -> Int -> [Int]
+removeMultiple [] _ = []
+removeMultiple (x:xs) a = if findremainder x a == 0
+							then (removeMultiple xs a)
+							else x:(removeMultiple xs a)
+
+
+outfitss lst1 lst2 lst3 lst4 = [(x,y,z,a) | x <- lst1, y <- lst2, z <- lst3, a <- lst4]
+
+honorifics l = [a | a <- l] ++ [a ++ " and " ++b| a <- l, b <- l]
+
+nato word = [snd(natoAlp !! x)  | y <- word, x <- [0..25] ,  y == fst(natoAlp !! x)]
+
 
 main = do
---putStrLn "Hello World"
+--mapM_ print militaryWord
 
---mapM_ print outfits
 
 putStr "The total number of possible outfits is: "
 print lengthOfOutfits
@@ -39,6 +77,8 @@ print lengthOfOutfits
 
 putStr "The total possible number of combinations of prefixes is :"
 print lengthOfPairs
+
+
 
 
 
