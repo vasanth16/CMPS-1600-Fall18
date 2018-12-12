53579153c17874d9a53f4da53e92f661d7e97084
vasanth16 <vasanthrajvr@gmail.com>
2018-12-11 20:35:43 -0600

    Finished Check point 3 with Late card

--- project3ios/project3ios/ViewController.swift
+++ project3ios/project3ios/ViewController.swift
@@ -12,15 +12,25 @@ class ViewController: UIViewController {
     @IBOutlet var nameField: UIView!
     @IBOutlet var newNameField: UITextField!
     @IBOutlet weak var sentenceField: UILabel!
+    @IBOutlet weak var weightField: UITextField!
+    
+    var calc = 0.0;
+    var heightsq = 0.0;
+    var temp = 0.0;
     override func viewDidLoad() {
         super.viewDidLoad()
         newNameField.delegate = self
+        weightField.delegate = self
         
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     @IBAction func addNameButton(_ sender: Any) {
-        sentenceField.text = "Hello \(newNameField.text!)"
+        temp = Double(newNameField.text!)!
+        heightsq = pow(temp,2)
+        calc = (Double(weightField.text!)!/heightsq) * 703
+        
+        sentenceField.text = "BMI: \(calc)"
     }
    
     
